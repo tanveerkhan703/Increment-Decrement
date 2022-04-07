98ab298db92c3f738c05e3c09b4f09ec2261cfe9
Tanveer Khan <97740446+tanveerkhan703@users.noreply.github.com>
2022-04-07 08:05:32 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let fuck = 0;
+
+function Add(){
+    document.getElementById('result').value = ++fuck;
+} 
+
+function Sub(){
+    document.getElementById('result').value = --fuck;
+}
+// function Remove(){
+//     document.getElementById('result').value = --'0';
+// }
